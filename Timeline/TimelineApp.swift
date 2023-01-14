b2aecd531535cbc775dba653f8a3a7074bcfbf55
Venky Venkatakrishnan <eeshwar1@gmail.com>
2023-01-13 18:22:23 -0600

    Initial Commit

---
+++ Timeline/TimelineApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimelineApp.swift
+//  Timeline
+//
+//  Created by Venkateswaran Venkatakrishnan on 1/13/23.
+//
+
+import SwiftUI
+
+@main
+struct TimelineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
