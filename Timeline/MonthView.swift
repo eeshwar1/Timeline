9fed540bfc4c4f98a4d4c86d88d5e4e304e6d020
Venky Venkatakrishnan <eeshwar1@gmail.com>
2023-01-13 22:40:17 -0600

    Added functionality to show events as a stack with count of events as a badge

--- Timeline/MonthView.swift
+++ Timeline/MonthView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct MonthView: View {
     
     @State var month: String
-    @State var monthHeight: CGFloat = 20
+    @State var monthHeight: CGFloat = 30
     @State var padding: CGFloat = 0
     
     @State var eventList: EventList = EventList()
@@ -35,18 +35,39 @@ struct MonthView: View {
             }
             .frame(width: 40, height: 20, alignment: .leading)
             
-           VStack {
+            VStack (spacing: -18){
+                  
                 ForEach(eventList.getEvents(), id:\.id) {
                     
                     event in
                     VStack {
-                        
-                        
+
                         Text(event.name).foregroundColor(.white)
                             .frame(width: 150, alignment: .center)
                             .padding(2)
                             .background(Color.brown).cornerRadius(5)
-                            .shadow(color: Color.gray, radius: 2, x: 2, y: 2)
+                            .shadow(color: Color.black, radius: 2, x: 2, y: -2)
+                            
+                            
+                    }
+                    
+                    
+                    
+                }
+                
+                // let eventCount = eventList.getEvents().count
+                
+                if (eventList.getEvents().count > 1) {
+                    
+                    ZStack {
+                        Circle()
+                            .stroke(.white)
+                            .background(Circle().fill(.red))
+                            .frame(width: 20, height: 20, alignment: .leading)
+                            .offset(.init(width: 75, height: -10))
+                        Text("\(eventList.getEvents().count)")
+                            .foregroundColor(Color.white)
+                            .offset(.init(width: 75, height: -10))
                     }
                     
                 }
